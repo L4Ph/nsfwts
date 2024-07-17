ae9d69847712c02b568b656e2ee8c6c76636e3be
Katsuyuki-Karasawa <4ranci0ne@gmail.com>
2024-07-17 22:04:16 +0900

    optimized code

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,27 @@
-import { Tensor } from "onnxruntime-web"
-import { processImageWithModel } from "./setup"
+import { Tensor } from "onnxruntime-web";
+import { processImageWithModel } from "./setup";
+import { NSFW_CLASSES } from "./nsfw_classes"
 
-const labels = ["drawings", "hentai", "neutral", "porn", "sexy"]
-const size = 224
+export type predictionType = {
+  className: (typeof NSFW_CLASSES)[keyof typeof NSFW_CLASSES];
+  probability: number;
+};
 
-export async function load(image: HTMLImageElement, modelUrl: string): Promise<Record<string, number>> {
-  const { session, input } = await processImageWithModel(image, modelUrl)
-  const tensor = new Tensor("float32", input, [1, size, size, 3])
-  const feeds: Record<string, Tensor> = { input_1: tensor }
-  const output = await session.run(feeds)
-  const outputData = output.dense_3.data as Float32Array
-  const results: Record<string, number> = {}
-  labels.forEach((label, index) => {
-    results[label] = outputData[index]
-  })
-  return results
+const size = 224;
+
+export async function load(image: HTMLImageElement, modelUrl: string): Promise<Array<predictionType>> {
+  const { session, input } = await processImageWithModel(image, modelUrl);
+  const tensor = new Tensor("float32", input, [1, size, size, 3]);
+  const feeds: Record<string, Tensor> = { input_1: tensor };
+  const output = await session.run(feeds);
+  const outputData = output.dense_3.data as Float32Array;
+
+  const predictions: Array<predictionType> = [];
+  outputData.forEach((probability, index) => {
+    predictions.push({
+      className: NSFW_CLASSES[index],
+      probability
+    });
+  });
+  return predictions;
 }
